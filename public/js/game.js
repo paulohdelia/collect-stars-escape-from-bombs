62d634ad43893af1ae58700bc68befcb4dc12a2b
paulohdelia <paulohdelia@gmail.com>
2020-10-05 16:02:19 -0300

    add scenario

--- public/js/game.js
+++ public/js/game.js
@@ -20,7 +20,7 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
-  this.load.image('ground', 'assets/platform.png');
+  this.load.image('ground', 'assets/ground.png');
   this.load.image('star', 'assets/star.png');
   this.load.image('bomb', 'assets/bomb.png');
   this.load.spritesheet('dude', 'assets/dude.png', {
@@ -33,6 +33,20 @@ function create() {
   const self = this;
 
   this.socket = io();
+
+  createScenario(this);
 }
 
 function update() {}
+
+function createScenario(self) {
+  self.add.image(400, 300, 'sky');
+
+  self.platforms = self.physics.add.staticGroup();
+
+  self.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  self.platforms.create(600, 400, 'ground');
+  self.platforms.create(50, 250, 'ground');
+  self.platforms.create(750, 220, 'ground');
+}
